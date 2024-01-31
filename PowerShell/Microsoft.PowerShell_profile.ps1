5ccf55b9265ebb5999212bbf46a62cb9008d78d2
illiatimoha <illiatimoshchuk.se@gmail.com>
2024-01-31 09:32:52 +0200

    PowerShell Config v1

---
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,34 @@
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+#Prompt
+oh-my-posh init pwsh | Invoke-Expression
+oh-my-posh init pwsh --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/main/themes/wholespace.omp.json' | Invoke-Expression
+#Aliases
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias vim nvim
+Set-Alias tt tree
+
+#Function
+
+function whereis ($command) {
+	Get-Command -Name $command -ErrorAction SilentlyContinue |
+	Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
+
+#Terminal Icons
+Import-Module Terminal-Icons
+
+#PSReadLine
+Import-Module PSReadLine
+Set-PSReadLineKeyHandler -Key Tab -Function Complete
+Set-PSReadLineOption -PredictionViewStyle ListView
+
